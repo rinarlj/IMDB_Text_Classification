aa26fddfae94a016f3af6b45d6771aed94627dac
Rina Ralijaona <116346251+rinarlj@users.noreply.github.com>
2024-12-17 18:02:51 +0300

    Update main_tfidf.py

--- main_tfidf.py
+++ main_tfidf.py
@@ -11,17 +11,12 @@ with open('/content/drive/MyDrive/imdb_train_test_text.pkl', 'rb') as f:
     train_labels = data['train_labels']
     test_labels = data['test_labels']
 
-# Initialiser le modèle TF-IDF
 model = TFIDFLog(max_features=10000)
 
-
-# Prétraitement
 X_train, X_test = model.preprocess(train_data, test_data)
 
-# Entraîner le modèle
 model.train(X_train, train_labels)
 
-# Évaluer le modèle
 accuracy = model.evaluate(X_test, test_labels)
 print(f"Accuracy with TF-IDF: {accuracy:.4f}")
 
